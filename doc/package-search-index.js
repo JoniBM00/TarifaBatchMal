fbc09798a544fcbfffa6ece63400a7309f779de6
Jonathan Becerro Martin <jonibm00@gmail.com>
2024-05-10 09:21:12 +0200

    docs: :memo: Javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.viewnext.tarifamicro"},{"l":"com.viewnext.tarifamicro.business"},{"l":"com.viewnext.tarifamicro.business.model"},{"l":"com.viewnext.tarifamicro.repository.configuration"},{"l":"com.viewnext.tarifamicro.repository.listener"}];updateSearchResults();
